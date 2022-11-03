694b8b4bfdecd281cad2d84f9e3eec1e159e6e25
Akil Selvan Rajendra Janarthanan <arajendr@ucsd.edu>
2022-11-03 15:40:17 -0700

    first screen first try

--- Front_End/EcoFood/screens/FirstScreen.js
+++ Front_End/EcoFood/screens/FirstScreen.js
@@ -1,11 +1,128 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, SafeAreaView, StatusBar, Image } from "react-native";
+import { Dimensions } from "react-native";
+import {
+    useFonts,
+    Quicksand_300Light,
+    Quicksand_400Regular,
+    Quicksand_500Medium,
+    Quicksand_600SemiBold,
+    Quicksand_700Bold,
+} from "@expo-google-fonts/quicksand";
 
 export default function FirstScreen() {
+    let [fontsLoaded] = useFonts({
+        Quicksand_700Bold,
+    });
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
     return (
-        <View style={styles.background}>
-            <View style={styles.header}></View>
-            <View style={styles.circle}></View>
-        </View>
+        <SafeAreaView style={styles.background}>
+            <StatusBar animated={true} backgroundColor="#000000" />
+            <SafeAreaView style={styles.header}>
+                <SafeAreaView
+                    style={{
+                        flex: 1,
+                        justifyContent: "center",
+                    }}
+                >
+                    <SafeAreaView
+                        style={{
+                            flexDirection: "row",
+                            justifyContent: "center",
+                        }}
+                    >
+                        <Image
+                            source={require("../images/logo.png")}
+                            style={{
+                                width: 50,
+                                height: 50,
+                            }}
+                        />
+                        <Text
+                            style={{
+                                fontSize: 36,
+                                alignSelf: "center",
+                                // center the text vertically
+                                textAlignVertical: "center",
+                                // center the text horizontally
+                                textAlign: "center",
+                                color: "#544B3d",
+                                fontFamily: "Quicksand_700Bold",
+                            }}
+                        >
+                            ECOFOOD
+                        </Text>
+                    </SafeAreaView>
+                </SafeAreaView>
+            </SafeAreaView>
+            <SafeAreaView
+                style={{
+                    paddingTop: "5%",
+                }}
+            >
+                <SafeAreaView style={styles.circle}>
+                    <Image
+                        source={require("../images/camera.png")}
+                        style={{
+                            // scaleX: (Dimensions.get("window").width * 0.0005),
+                            // scaleY: Dimensions.get("window").width * 0.0005,
+                            width: Dimensions.get("window").width * 0.4,
+                            height: Dimensions.get("window").width * 0.4,
+                        }}
+                        resizeMode="contain"
+                    />
+                </SafeAreaView>
+                <Text
+                    style={{
+                        fontSize: 36,
+                        alignSelf: "center",
+                        // center the text vertically
+                        textAlignVertical: "center",
+                        // center the text horizontally
+                        textAlign: "center",
+                        color: "#544B3d",
+                        fontFamily: "Quicksand_700Bold",
+                    }}
+                >
+                    Take a photo!
+                </Text>
+            </SafeAreaView>
+            <SafeAreaView
+                style={{
+                    paddingTop: "5%",
+                }}
+            >
+                <SafeAreaView style={styles.circle}>
+                    <Image
+                        source={require("../images/notes.png")}
+                        style={{
+                            // scaleX: (Dimensions.get("window").width * 0.0005),
+                            // scaleY: Dimensions.get("window").width * 0.0005,
+                            width: Dimensions.get("window").width * 0.4,
+                            height: Dimensions.get("window").width * 0.4,
+                        }}
+                        resizeMode="contain"
+                    />
+                </SafeAreaView>
+                <Text
+                    style={{
+                        fontSize: 36,
+                        alignSelf: "center",
+                        // center the text vertically
+                        textAlignVertical: "center",
+                        // center the text horizontally
+                        textAlign: "center",
+                        color: "#544B3d",
+                        fontFamily: "Quicksand_700Bold",
+                    }}
+                >
+                    Text Entry
+                </Text>
+            </SafeAreaView>
+        </SafeAreaView>
     );
 }
 
@@ -19,15 +136,21 @@ const styles = StyleSheet.create({
         backgroundColor: "#BBBE64",
         width: "100%",
         height: "12%",
-        borderBottomLeftRadius: 30,
-        borderBottomRightRadius: 30,
+        borderBottomLeftRadius: Dimensions.get("window").height * 0.08,
+        borderBottomRightRadius: Dimensions.get("window").height * 0.08,
 
+        // borderBottomLeftRadius: "15%",
+        // borderBottomRightRadius: "15%",
     },
     circle: {
         backgroundColor: "#BBBE64",
-        width: 100,
-        height: 100,
-        borderRadius: 50,
+        width: Dimensions.get("window").width * 0.6,
+        aspectRatio: 1,
+        borderRadius: Dimensions.get("window").width * 0.3,
+        // borderRadius: 1000,
+        alignSelf: "center",
+        justifyContent: "center",
+        alignItems: "center",
     },
     container: {
         flex: 1,
